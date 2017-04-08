9caf826a06920940d094e33616d50153d0a0681b
Simon Reggiani <simon.reggiani@gmail.com>
2017-04-08 13:54:15 -0700

    INIT

---
+++ app/src/main/java/com/sregg/surfaceviewdemo/MySurfaceView.java
@@ -0,0 +1,71 @@
+package com.sregg.surfaceviewdemo;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+
+public class MySurfaceView extends SurfaceView implements SurfaceHolder.Callback {
+    private static final String TAG = "MySurfaceView";
+
+    private Paint paint;
+
+    public MySurfaceView(Context context) {
+        super(context);
+        init();
+    }
+
+    public MySurfaceView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public MySurfaceView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paint.setStyle(Paint.Style.FILL);
+        paint.setColor(Color.GREEN);
+
+        getHolder().addCallback(this);
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        tryDrawing(holder);
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+
+    }
+
+    private void tryDrawing(SurfaceHolder holder) {
+        Log.i(TAG, "Trying to draw...");
+
+        Canvas canvas = holder.lockCanvas();
+        if (canvas == null) {
+            Log.e(TAG, "Cannot draw onto the canvas as it's null");
+        } else {
+            drawMyStuff(canvas);
+            holder.unlockCanvasAndPost(canvas);
+        }
+    }
+
+    private void drawMyStuff(Canvas canvas) {
+        canvas.drawCircle(getWidth() / 2, getHeight() / 2, 300, paint);
+    }
+}
